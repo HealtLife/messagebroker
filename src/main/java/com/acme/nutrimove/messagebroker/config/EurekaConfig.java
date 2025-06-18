f86b9d8b56f3a364dcef57733ab9a6d12306159a
joseluza <u202213404@upc.edu.pe>
2025-06-18 09:55:26 -0500

    Update POM

--- src/main/java/com/acme/nutrimove/messagebroker/config/EurekaConfig.java
+++ src/main/java/com/acme/nutrimove/messagebroker/config/EurekaConfig.java
@@ -1,10 +1,10 @@
 package com.acme.nutrimove.messagebroker.config;
 
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;  // Importación correcta
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableEurekaClient
+@EnableEurekaClient  // Habilita el registro en Eureka
 public class EurekaConfig {
     // Esta clase permite registrar el microservicio en Eureka
 }
