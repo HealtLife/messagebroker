b6a53a6552f14c18c6728050e9601d38512ada44
Nato0211 <nato0211@gmail.com>
2025-06-18 14:27:31 -0500

    feat: feign added

--- src/main/java/com/acme/nutrimove/messagebroker/MessageBrokerApplication.java
+++ src/main/java/com/acme/nutrimove/messagebroker/MessageBrokerApplication.java
@@ -2,10 +2,11 @@ package com.acme.nutrimove.messagebroker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableFeignClients
 public class MessageBrokerApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/acme/nutrimove/messagebroker/config/EurekaConfig.java
+++ src/main/java/com/acme/nutrimove/messagebroker/config/EurekaConfig.java
@@ -1,10 +1,11 @@
 package com.acme.nutrimove.messagebroker.config;
 
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;  // Importación correcta
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableEurekaClient  // Habilita el registro en Eureka
+@EnableFeignClients
 public class EurekaConfig {
     // Esta clase permite registrar el microservicio en Eureka
 }
